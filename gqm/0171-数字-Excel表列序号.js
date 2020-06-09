0c2ca6f88c8e8e70a058f382079081fe6d7f8d46
gaoqiming <gaoqiming@100tal.com>
2020-06-09 23:21:28 +0800

    feat: 每日一题打卡，补上周题

--- gqm/0171-数字-Excel表列序号.js
+++ gqm/0171-数字-Excel表列序号.js
@@ -11,4 +11,24 @@
     AA -> 27
     AB -> 28 
     ...
+
+    其实就是 二十六进制数转为十进制数。
  */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const titleToNumber = (s) => {
+  var arr = s.split(""),
+  result = 0,
+  beishu = 1;
+ 
+  while(arr.length !== 0){
+      result += (arr.pop().charCodeAt() - 64) * beishu;
+      beishu *= 26;
+  }
+  return result;
+};
+
+console.log(titleToNumber('AB'))
+console.log(titleToNumber('AA'))
--- gqm/0412-数学-Fizz Buzz.js
+++ gqm/0412-数学-Fizz Buzz.js
@@ -9,3 +9,25 @@
   3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
 
  */
+
+ /**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = (n) => {
+  const result = []
+  for(let i = 1; i <= n; i++) {
+    if(i % 3 === 0 && i % 5 === 0){
+      result.push('FizzBuzz')
+    }else if(i % 3 === 0){
+      result.push('Fizz')
+    }else if(i % 5 === 0){
+      result.push('Buzz')
+    }else {
+      result.push(i + '');
+    }
+  }
+  return result
+};
+
+console.log(fizzBuzz(15))
