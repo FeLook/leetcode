00690fdbce14f462d4a22e89df0bb74345e2f920
高启明 <gaoqiming@100tal.com>
2020-07-27 14:21:43 +0800

    feat: 完成数组-二维数组查找

---
+++ gqm/04-数组-二维数组中的查找.js
@@ -0,0 +1,54 @@
+/** 
+ * 在一个 n * m 的二维数组中，每一行都按照从左到右递增的顺序排序，每一列都按照从上到下递增的顺序排序。请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+
+示例:
+
+现有矩阵 matrix 如下：
+
+[
+  [1,   4,  7, 11, 15],
+  [2,   5,  8, 12, 19],
+  [3,   6,  9, 16, 22],
+  [10, 13, 14, 17, 24],
+  [18, 21, 23, 26, 30]
+]
+给定 target = 5，返回 true。
+
+给定 target = 20，返回 false。
+
+  限制：
+
+  0 <= n <= 1000
+
+  0 <= m <= 1000
+*/
+
+const search = function (matrix, target) {
+  if (matrix.length === 0) return false
+  let rows = matrix.length, columns = matrix[0].length;
+  let row = 0, column = columns - 1;
+  while (row < rows && column >= 0) {
+      const num = matrix[row][column];
+      if (num == target) {
+          return true;
+      } else if (num > target) {
+          column--;
+      } else {
+          row++;
+      }
+  }
+  return false;
+};
+
+const search1 = (matrix, target) => {
+  return matrix.flat(1).includes(target)
+}
+const arr = [
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+]
+console.log(search(arr, 3))
+console.log(search1(arr, 3))
+console.log(search1(arr, 10))
+
