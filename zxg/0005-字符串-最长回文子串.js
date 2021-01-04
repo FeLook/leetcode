1a825dd11435f83012cc630654fae99baf11cf0c
xiamer_10 <5032651@qq.com>
2021-01-04 18:36:33 +0800

    feat: 0005-字符串-最长回文子串

--- zxg/0005-字符串-最长回文子串.js
+++ zxg/0005-字符串-最长回文子串.js
@@ -1,4 +1,10 @@
 /*
+ * @Author: xiaoguang_10@qq.com
+ * @LastEditors: xiaoguang_10@qq.com
+ * @Date: 2020-06-03 00:31:13
+ * @LastEditTime: 2021-01-04 18:31:45
+ */
+/*
   给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
 
   示例 1：
@@ -11,3 +17,20 @@
   输入: "cbbd"
   输出: "bb"
 */
+
+/**
+ * @param {string} str
+ * @return {string}
+ */
+var longestPalindrome = function(str) {
+  if (typeof str !== 'string') return
+  let arr = []
+  for (let index = 0; index < str.length; index++) {
+    const singStr = str.charAt(index)
+    if (arr.includes(singStr)) {
+      return str.substring(arr.indexOf(singStr), index + 1)
+    } else {
+      arr.push(singStr)
+    }
+  }
+};
